ec6fc7695d20c3d9a22eb2f1fa861219793433fb
VALISOA <herimalala.val@gmail.com>
2021-09-08 00:25:14 +0200

    updatedAt

--- src/DataPersister/ArticleDataPersister.php
+++ src/DataPersister/ArticleDataPersister.php
@@ -6,6 +6,8 @@ namespace App\DataPersister;
 
 use App\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
 
@@ -15,14 +17,25 @@ class ArticleDataPersister implements ContextAwareDataPersisterInterface
      * @var EntityManagerInterface
      */
     private $_entityManager;
+
+    /**
+     * @param SluggerInterface
+     */
     private $_slugger;
+
+    /**
+     * @param Request
+     */
+    private $_request;
 
     public function __construct(
         EntityManagerInterface $entityManager,
-        SluggerInterface $slugger
+        SluggerInterface $slugger,
+        RequestStack $request
     ) {
         $this->_entityManager = $entityManager;
         $this->_slugger = $slugger;
+        $this->_request = $request->getCurrentRequest();
     }
 
     /**
@@ -38,11 +51,16 @@ class ArticleDataPersister implements ContextAwareDataPersisterInterface
      */
     public function persist($data, array $context = [])
     {
-        $data->setSlug(
-            $this
-                ->_slugger
-                ->slug(strtolower($data->getTitle())). '-' .uniqid()
-        );
+        if(!$data->getIsPublished()){
+            $data->setSlug(
+                $this
+                    ->_slugger
+                    ->slug(strtolower($data->getTitle())). '-' .uniqid()
+            );
+        }
+        if ($this->_request->getMethod() !== 'POST') {
+            $data->setUpdatedAt(new \DateTime());
+        }
 
         $this->_entityManager->persist($data);
         $this->_entityManager->flush();
